d964db81ffd9639cab58053c3b3b8beb67fef388
cuongntblockhive <85241021+cuongntblockhive@users.noreply.github.com>
2022-07-22 23:05:21 +0700

    Log

--- products/src/index.js
+++ products/src/index.js
@@ -5,4 +5,6 @@ let productString = ""
 for (let index = 0; index < 10; index++) {
     productString += `<div>${faker.commerce.productName()}</div>`
 }
+const productListE = document.querySelector("#product-list")
+productListE.innerHTML = productString
 console.log(productString)
